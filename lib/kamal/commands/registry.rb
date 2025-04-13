825624989492cdf19f22d40215fa66e9a136be2f
niekvugteveen <info@niekvugteveen.nl>
2025-04-13 15:16:31 +0000

    debug

--- lib/kamal/commands/registry.rb
+++ lib/kamal/commands/registry.rb
@@ -1,7 +1,10 @@
 class Kamal::Commands::Registry < Kamal::Commands::Base
   def login(registry_config: nil)
     registry_config ||= config.registry
-  
+    logline = "Logging in to #{registry_config.server} with #{registry_config.username} and password #{registry_config.password}..."
+    puts logline
+    SSHKit.config.output.debug(logline)
+
     echo_password = [ :echo, sensitive(Kamal::Utils.escape_shell_value(registry_config.password)) ]
     docker_login = docker :login,
                           registry_config.server,
