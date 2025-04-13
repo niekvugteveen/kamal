470424f13f84ee6673d2fc1bdb26b8e3b354df37
niekvugteveen <info@niekvugteveen.nl>
2025-04-13 14:13:54 +0000

    Use the correct docker login method

--- lib/kamal/commands/registry.rb
+++ lib/kamal/commands/registry.rb
@@ -1,11 +1,14 @@
 class Kamal::Commands::Registry < Kamal::Commands::Base
   def login(registry_config: nil)
     registry_config ||= config.registry
-
-    docker :login,
-      registry_config.server,
-      "-u", sensitive(Kamal::Utils.escape_shell_value(registry_config.username)),
-      "-p", sensitive(Kamal::Utils.escape_shell_value(registry_config.password))
+  
+    echo_password = [ :echo, sensitive(Kamal::Utils.escape_shell_value(registry_config.password)) ]
+    docker_login = docker :login,
+                          registry_config.server,
+                          "-u", sensitive(Kamal::Utils.escape_shell_value(registry_config.username)),
+                          "--password-stdin"
+  
+    pipe echo_password, docker_login
   end
 
   def logout(registry_config: nil)
